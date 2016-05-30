a513f23c4958798c6e63d20784e4d6271f0319c8
Hanspeter Portner <dev@open-music-kontrollers.ch>
2016-05-30 17:15:26 +0200

    win: add -liphlpapi.

--- cmake/x86_64-w64-mingw32.cmake
+++ cmake/x86_64-w64-mingw32.cmake
@@ -22,6 +22,6 @@ set(STATIC_UV "/opt/${TOOLCHAIN}/lib/libuv.a")
 set(STATIC_SRATOM "/opt/${TOOLCHAIN}/lib/libsratom-0.a")
 set(STATIC_SERD "/opt/${TOOLCHAIN}/lib/libserd-0.a")
 set(STATIC_SORD "/opt/${TOOLCHAIN}/lib/libsord-0.a")
-set(LIBS ${LIBS} "-static-libgcc -lws2_32 -lpsapi -Wl,-Bstatic -lwinpthread -luserenv")
+set(LIBS ${LIBS} "-static-libgcc -lws2_32 -lpsapi -liphlpapi -Wl,-Bstatic -lwinpthread -luserenv")
 
 set(WINE wine64)
